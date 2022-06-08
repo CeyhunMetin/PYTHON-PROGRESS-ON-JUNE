5198b9ad818a13c462abdc283fee359e70ecc90b
ceyhunmetin <ceyhunmetin@hotmail.de>
2022-06-08 13:04:54 -0700

    first commit

---
+++ Calculator3.py
@@ -0,0 +1,41 @@
+print ("""
+
+
+
+Welcome to Calculator 3.0
+
+
+""")
+a= float (input("Please enter first number:"))
+b= float (input("Please enter second number:"))
+
+operation=input("Which operation would you like to do?(+,-,*,/):")
+
+def acc(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mltp(a,b):
+    return a*b
+
+def dvd(a,b):
+    return (a/b)
+
+if (operation == "+"):
+    print(acc(a,b))
+
+elif (operation=="-"):
+    print(sub(a,b))
+
+elif (operation == "*"):
+    print(mltp(a,b))
+
+elif (operation == "/"):
+    print(dvd(a,b))
+   
+
+else:
+    print ("Wrong operation entered.")      
+
